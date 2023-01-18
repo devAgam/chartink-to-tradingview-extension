75e2853c102fe740d2212a9771bd93b9acb642a2
Agamjot Singh Jashan <tippytactics@gmail.com>
2023-01-18 21:53:48 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+  var links = document.querySelectorAll('a[href^="/stocks"]');
+  for (var i = 0; i < links.length; i++) {
+    console.log(links[i].href);
+    const baseUrl = "https://chartink.com/stocks/";
+    links[i].href =
+      "https://in.tradingview.com/chart/?symbol=NSE:" +
+      links[i].href.substring(baseUrl.length).replace(".html", "");
+  }
+};
