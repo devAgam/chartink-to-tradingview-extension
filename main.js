8c9ea33f7253eb62350a1630401f802b9a6b2393
Agamjot Singh Jashan <tippytactics@gmail.com>
2023-01-24 13:38:58 +0530

    code cleanup and fix dom revert issue on chartink auto refresh

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 window.onload = function () {
+  changeURL();
+};
+
+function changeURL() {
   var links = document.querySelectorAll('a[href^="/stocks"]');
   for (var i = 0; i < links.length; i++) {
     const baseUrl = "https://chartink.com/stocks/";
@@ -6,4 +10,19 @@ window.onload = function () {
       "https://in.tradingview.com/chart/?symbol=NSE:" +
       links[i].href.substring(baseUrl.length).replace(".html", "");
   }
+}
+
+var observer = new MutationObserver(function (mutations) {
+  mutations.forEach(function (mutation) {
+    setTimeout(function () {
+      changeURL();
+    }, 100);
+  });
+});
+
+var config = {
+  childList: true,
+  subtree: true,
 };
+
+observer.observe(document.body, config);
