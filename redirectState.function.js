5c551ca856bf446b0d06b12ba74fb0fbc3274799
devAgam <tippytactics@gmail.com>
2023-06-29 03:36:20 -0700

    function which handles the state and change of the redirect checkbox
    (i know the name conventions are a little messed but its a quick fix and its 3:36AM while i'm writting this code)

---
+++ redirectState.function.js
@@ -0,0 +1,39 @@
+function attachToRedirectStateSetting() {
+  var redirectStateSetting = document.getElementById("redirect");
+  // redirect element is a input with type checkbox, if its checked then send message to background script
+  // to set chartRedirect state to true
+  redirectStateSetting.addEventListener("change", function () {
+    if (this.checked) {
+      chrome.runtime.sendMessage({
+        message: "setChartRedirectState",
+        state: true,
+      });
+    } else {
+      chrome.runtime.sendMessage({
+        message: "setChartRedirectState",
+        state: false,
+      });
+    }
+  });
+}
+
+// execute the function on dom content loaded
+document.addEventListener("DOMContentLoaded", attachToRedirectStateSetting);
+
+function updateCheckBoxState() {
+  // get the state and update the checkbox
+  chrome.runtime.sendMessage(
+    { message: "getChartRedirectState" },
+    function (response) {
+      console.log(response);
+      if (response.chartRedirectState) {
+        document.getElementById("redirect").defaultChecked = true;
+      } else {
+        document.getElementById("redirect").defaultChecked = false;
+      }
+    }
+  );
+}
+
+// execute the function on dom content loaded
+document.addEventListener("DOMContentLoaded", updateCheckBoxState);
